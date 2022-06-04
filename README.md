0a490c1fff7de91372e14c270c78512c8066e70f
LuukvE <l.vanegeraat@gmail.com>
2022-06-04 20:28:34 +0100

    Feat: Add Firebase deployment steps and config

--- README.md
+++ README.md
@@ -8,3 +8,16 @@ Modern blueprints for TypeScript web development.
 2. `$ npm install`
 3. `$ npm start`
 4. `$ npm run server`
+
+## Firebase Deploy Steps
+
+1. Go to [firebase.google.com](https://firebase.google.com) and sign in to your Google account
+2. Create a "Web" project
+3. `$ npm install firebase`
+4. `$ npx firebase login`
+5. `$ npx firebase init hosting` - change public folder to `build`
+6. Add deploy script to `package.json`
+7. `$ npm run build`
+8. `$ npm run deploy`
+9. (optional) Go to Project Settings in Firebase and copy over Analytics code to `index.ts`
+10. (optional) Add Custom domain, prove ownership by adding TXT record, then add A record
--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,8 @@ import '@fortawesome/fontawesome-free/js/all';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
 import { createBrowserHistory } from 'history';
 import { Router, Switch, Route, Redirect, NavLink } from 'react-router-dom';
 
@@ -11,7 +13,21 @@ import './index.scss';
 import store from './store';
 import Welcome from './components/Welcome';
 
+const firebaseConfig = {
+  apiKey: 'AIzaSyBsm2wz3ahIgqvqiLgrFPNtwblojFknJQA',
+  authDomain: 'apex-software-engineering.firebaseapp.com',
+  projectId: 'apex-software-engineering',
+  storageBucket: 'apex-software-engineering.appspot.com',
+  messagingSenderId: '572890238748',
+  appId: '1:572890238748:web:a10acacea13961d19ff4d9',
+  measurementId: 'G-STQ12VT7GF'
+};
+
 const history = createBrowserHistory();
+
+const app = initializeApp(firebaseConfig);
+
+getAnalytics(app);
 
 ReactDOM.render(
   <Router history={history}>

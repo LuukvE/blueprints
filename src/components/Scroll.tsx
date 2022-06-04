44d214b42f4e2cb7d4498de2ee604471c34c47f2
LuukvE <l.vanegeraat@gmail.com>
2022-06-04 20:57:02 +0100

    Feat: Configure animate on scroll library

---
+++ src/components/Scroll.tsx
@@ -0,0 +1,26 @@
+import React, { FC } from 'react';
+
+import './Scroll.scss';
+
+const Scroll: FC = () => {
+  return (
+    <main className="Scroll">
+      <img src="/logo.png" alt="" data-aos="fade-up" />
+      <img src="/logo.png" alt="" data-aos="fade-up" />
+      <img src="/logo.png" alt="" data-aos="fade-up" />
+      <img src="/logo.png" alt="" data-aos="fade-up" />
+      <img src="/logo.png" alt="" data-aos="fade-up" />
+      <img src="/logo.png" alt="" data-aos="flip-left" />
+      <img src="/logo.png" alt="" data-aos="flip-right" />
+      <img src="/logo.png" alt="" data-aos="zoom-in" />
+      <img src="/logo.png" alt="" data-aos="zoom-in" />
+      <img src="/logo.png" alt="" data-aos="zoom-in" />
+      <img src="/logo.png" alt="" data-aos="zoom-in" />
+      <img src="/logo.png" alt="" data-aos="zoom-in" />
+      <img src="/logo.png" alt="" data-aos="fade-down" />
+      <img src="/logo.png" alt="" data-aos="fade-down" />
+    </main>
+  );
+};
+
+export default Scroll;
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
+import 'aos/dist/aos.css';
 import 'react-app-polyfill/ie11';
 import '@fortawesome/fontawesome-free/js/all';
 
+import AOS from 'aos';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
@@ -9,7 +11,10 @@ import { Router, Switch, Route, Redirect, NavLink } from 'react-router-dom';
 
 import './index.scss';
 import store from './store';
+import Scroll from './components/Scroll';
 import Welcome from './components/Welcome';
+
+AOS.init();
 
 const history = createBrowserHistory();
 
@@ -20,10 +25,14 @@ ReactDOM.render(
         <NavLink exact to="/">
           Welcome
         </NavLink>
+        <NavLink to="/scroll">Scroll</NavLink>
       </nav>
       <Switch>
         <Route exact path="/">
           <Welcome />
+        </Route>
+        <Route path="/scroll">
+          <Scroll />
         </Route>
         <Redirect to="/" />
       </Switch>
